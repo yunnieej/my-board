d824101682bcc5d6014d500825e6d707311682de
yunnieej <yunnieej@gmail.com>
2023-07-02 22:11:18 +0900

    first commit

---
+++ src/main/java/project/myboard/MyBoardApplication.java
@@ -0,0 +1,13 @@
+package project.myboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBoardApplication.class, args);
+	}
+
+}
